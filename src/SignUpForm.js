9b0281b11f604279850a556c0e8fed7a9fc69f54
Christian Cruz <christian.cruz92@hotmail.com>
2024-08-02 10:03:04 -0400

    added the form and fields to the SignupForm component.

--- src/SignUpForm.js
+++ src/SignUpForm.js
@@ -1,7 +1,125 @@
-import React from "react";
+import React, {useState} from "react";
+// import useLocalStorageState from './hooks/useLocalStorageState';
+import {useNavigate} from "react-router-dom"
+import {
+    Card,
+    CardBody,
+    CardTitle,
+    CardText,
+    ListGroup,
+    ListGroupItem
+  } from "reactstrap";
 
-const SignUpForm = () => {
+const SignupForm = () => {
+    const navigate = useNavigate();
+    const INITIAL_STATE = {
+        username: "",
+        password: "",
+        firstName: "",
+        lastName: "",
+        email: "",
+        birthday: "",
+    };
 
+    // use state to control the form
+    const [formData, setFormData] = useState(INITIAL_STATE);
+    const [formErrors, setFormErrors] = useState([])
+
+    // const[token, setToken] = useLocalStorageState('token', '')
+
+    // allow the changes to the form to be entered into state 
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormData(formData => ({
+            ...formData,
+            [name]: value
+        }))
+    }
+
+    // submit the form if the form is correct, otherwise alert them it's not
+    async function handleSubmit(e) {
+        e.preventDefault();
+        console.log("submitted form")
+        // const result = await handleUserAuth(formData, 'register');
+        // if (result.success) {
+        //   navigate("/");
+        // } else {
+        //   setFormErrors(result.errors);
+        // }
+      }
+
+    return(
+        <div className="formContainer">
+            <Card>
+                <CardBody>
+                    <h1>Sign Up</h1>
+
+                    <form 
+                    className="signUpForm" 
+                    onSubmit={handleSubmit}
+                    >
+                        
+                        <label htmlFor="username">Username</label>
+                        <input 
+                        id="username"
+                        type="text"
+                        name="username"
+                        value={formData.username}
+                        onChange={handleChange}
+                        />
+                        <br></br>                
+                        <label htmlFor="password">Password</label>
+                        <input 
+                        id="password"
+                        type="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                        />
+                        <br></br>
+                        
+                        <label htmlFor="firstName">First name</label>
+                        <input 
+                        id="firstName"
+                        type="text"
+                        name="firstName"
+                        value={formData.firstName}
+                        onChange={handleChange}
+                        />
+                        <br></br>
+                        <label htmlFor="lastName">Last name</label>
+                        <input 
+                        id="lastName"
+                        type="text"
+                        name="lastName"
+                        value={formData.lastName}
+                        onChange={handleChange}
+                        />
+                        <br></br>
+                        <label htmlFor="email">Email</label>
+                        <input 
+                        id="email"
+                        type="text"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        />
+                        <br></br>
+                        <label htmlFor="birthday">Birthday</label>
+                        <input 
+                        id="birthday"
+                        type="date"
+                        name="birthday"
+                        value={formData.birthday}
+                        onChange={handleChange}
+                        />
+                        <br></br>
+                        <button>Submit</button>
+                    </form>
+                </CardBody>
+            </Card>
+        </div>
+    )
 }
 
-export default SignUpForm;
+export default SignupForm;
